8e7bfcb5ea44a962c0f9d74da2d0127d28bcf23c
AHJ388 <amarchandhooda40@gmail.com>
2023-01-01 22:06:25 +0530

    Done

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch("https://aichat-7kcv.onrender.com", {
+    const response = await fetch("https://aichat-df8z.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
